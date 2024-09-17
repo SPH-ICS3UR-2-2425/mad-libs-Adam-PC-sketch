1a80155658b14f9a4ef53e526d108c2e72c26f1e
Adam-PC-sketch <adam.dagher@stu.ocsb.ca>
2024-09-17 14:36:32 -0400

    adadadad

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		/*
 		 * MadLibs Program
-		 * Author: 
+		 * Author:Adam Dagher 
 		 * Class: ICS3U
 		 * Date: Sept. 16, 2024
 		 * Make a program that asks for 10 inputs
@@ -14,10 +14,15 @@ public class Main {
 		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Welcome to Mad Libs!");
-		System.out.println("What's your name?");
+		System.out.println("Give me a name");
 		String name = in.nextLine();
-		System.out.println("One day, "+name+" wrote a Mad Lib in class!");
-
+		System.out.println("Give me a number");
+		
+		int num1=in.nextInt();
+        System.out.println("One day, "+name+" wrote a Mad Lib in class!");
+	System.out.println("Yesterday there was a soccer game");
+	//flush
+	in.nextLine();
+	System.out.println("");
 	}
-
 }
